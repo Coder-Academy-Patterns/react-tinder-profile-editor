a2546c82ac909049dd940ca792e083fecff3b7dd
Patrick Smith <patrick@burntcaramel.com>
2017-11-06 16:19:22 +1100

    Change to store profile info in state

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import './App.css';
 import Profile from './components/Profile'
 
 class App extends Component {
+  state = {
+    firstName: 'Bob',
+    lastName: 'Jones',
+    imageURL: 'https://randomuser.me/api/portraits/men/83.jpg'
+  }
+
   render() {
+    const { firstName, lastName, imageURL } = this.state
+
     return (
       <div className="App">
-        <Profile firstName='Bob' lastName='Jones' imageURL='https://randomuser.me/api/portraits/men/83.jpg' />
+        <Profile firstName={ firstName } lastName={ lastName } imageURL={ imageURL } />
       </div>
     );
   }
