cff83ec54dfa3900dd44f3ff32c30b4920ac68fc
Patrick Smith <patrick@burntcaramel.com>
2017-11-06 16:27:01 +1100

    Add first name field

--- src/App.js
+++ src/App.js
@@ -9,12 +9,25 @@ class App extends Component {
     imageURL: 'https://randomuser.me/api/portraits/men/83.jpg'
   }
 
+  onChangeFirstName = (event) => {
+    const input = event.target // Get text field
+    const newString = input.value // Current value in text field
+    // setState will re-render our app with new state
+    this.setState({
+      firstName: newString // Change firstName to this new value
+    })
+  }
+
   render() {
     const { firstName, lastName, imageURL } = this.state
 
     return (
       <div className="App">
         <Profile firstName={ firstName } lastName={ lastName } imageURL={ imageURL } />
+        <label>
+          First name:
+          <input value={ firstName } onChange={ this.onChangeFirstName } />
+        </label>
       </div>
     );
   }
